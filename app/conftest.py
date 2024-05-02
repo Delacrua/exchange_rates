98fe1c4caae079fd334d0cc7d71debc0a41c20bf
Uladzimir Fiodarau <ufiodarau@gmail.com>
2024-05-02 09:56:16 +0300

    Worked on project overall: - edited fixture to remove warning

--- app/conftest.py
+++ app/conftest.py
@@ -6,14 +6,14 @@ from app.exchange_rates.routes import setup_routes
 
 
 @pytest.fixture
-async def healthcheck_test_client(loop, aiohttp_client):
+async def healthcheck_test_client(event_loop, aiohttp_client):
     app = web.Application()
     oas.setup(app, url_prefix="/docs")
     return await aiohttp_client(app)
 
 
 @pytest.fixture
-async def exchange_rate_test_client(loop, aiohttp_client):
+async def exchange_rate_test_client(event_loop, aiohttp_client):
     app = web.Application()
     setup_routes(app)
     return await aiohttp_client(app)
